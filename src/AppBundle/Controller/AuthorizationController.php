ed8dd4a5fd1accbb885e77030cea204290eb7f85
spritex123 <spritex123@mail.ru>
2016-12-20 15:31:08 +0600

    Add Authorization

---
+++ src/AppBundle/Controller/AuthorizationController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+//use AppBundle\Entity\User;
+//use AppBundle\Form\AuthorizationType;
+
+class AuthorizationController extends Controller
+{
+    /**
+     * @Route("/authorization", name="authorization")
+     */
+    public function authorizationAction(Request $request)
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('AppBundle:Authorization:index.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ));
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logoutAction()
+    {
+        throw new \Exception('@Route("/logout", name="logout")');
+    }
+}
